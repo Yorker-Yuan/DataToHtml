b88d1b6ba26d4aed22927fa9e518d6686e7a9374
YUAN <yankees2484@gmail.com>
2020-12-13 12:21:48 +0800

    畫面修改css

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,5 @@
-const areaSelect = document.querySelector("#area");
-const zoneSelect = document.querySelector("#zone");
+const areaSelect = document.querySelector("#select__area");
+const zoneSelect = document.querySelector("#select__zone");
 const box = document.querySelector("#box");
 const endpoint = 'public/data.json';
 
@@ -8,20 +8,44 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var rows = JSON.parse(xhttp.responseText);
-    var txtArea = `<option value="">請選擇行政區域</option>`;
-    var txtZone = `<option value="">請選擇鄉鎮區</option>`;
-    var result = '';
     
     //去重
     const set = new Set();
     const NorepeatCity = rows.filter(item=>!set.has(item.City)?set.add(item.City):false);
-    //一階SELECT
-    for (let i = 0;i<NorepeatCity.length;i++) {
-      txtArea += `<option value="${NorepeatCity[i].City}">${NorepeatCity[i].City}</option>`;
-    }   
-    document.getElementById("area").innerHTML = txtArea;
+   
+    ///二階select
+    var zoneval = changeHandler();
+    var zoneSets = rows.filter(function (set){
+      return set.Zone == zoneval;
+    });
+    console.log('zoneval:',zoneval);
+    console.log('zoneSets:',zoneSets);
+    //三階畫面
+    var viewSets = changeViewHandler();
+    
     //全部顯示
-    for(let j = 0;j<rows.length; j++)
+    show(NorepeatCity,zoneSets,rows);
+  }
+};
+xhttp.open("GET", 'public/data.json', true);
+xhttp.send();
+
+function show(area,zone,rows){
+  var txtArea = `<option value="">請選擇行政區域</option>`;
+  var txtZone = `<option value="">請選擇鄉鎮區</option>`;
+  var result = '';
+
+  for (let i = 0;i<area.length;i++) {
+    txtArea += `<option value="${area[i].City}">${area[i].City}</option>`;
+  }   
+  areaSelect.innerHTML = txtArea;
+
+  // for (let i = 0;i<zone.length;i++) {
+  //   txtZone += `<option value="${zone[i].Zone}">${zone[i].Zone}</option>`;
+  // }   
+  // areaSelect.innerHTML = txtZone;
+
+  for(let j = 0;j<rows.length; j++)
     {
       result += `
       <div class="col">
@@ -38,16 +62,12 @@ xhttp.onreadystatechange = function() {
         </div>
       `;
     }
-    document.getElementById("box").innerHTML = result;
-
-  }
-};
-xhttp.open("GET", 'public/data.json', true);
-xhttp.send();
+    box.innerHTML = result;
+}
 
 //二階select
 function changeHandler(val){
-  
+  return val;
 }
 //三階畫面
 function changeViewHandler(val){
