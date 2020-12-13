37fd04335eee3fe06a6fd9f0a61cef634f9752bd
YUAN <yankees2484@gmail.com>
2020-12-13 16:02:52 +0800

    loading

--- js/Script.js
+++ js/Script.js
@@ -6,7 +6,6 @@ const box = document.querySelector("#box");
 const endpoint = 'public/data.json';
 
 var xhttp = new XMLHttpRequest();
-
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     rows = JSON.parse(xhttp.responseText);
@@ -20,10 +19,6 @@ xhttp.onreadystatechange = function() {
     txtArea += `<option value="${NorepeatCity[i].City}">${NorepeatCity[i].City}</option>`;
   }   
   areaSelect.innerHTML = txtArea;
-
-    //三階畫面
-    var viewSets = changeViewHandler();
-
     //全部顯示
     show(rows);
   }
@@ -33,7 +28,6 @@ xhttp.send();
 
 function show(rows){
   var result = '';
-
   for(let j = 0;j<rows.length; j++)
     {
       result += `
@@ -62,7 +56,6 @@ function changeHandler(val){
    //去重
    const set = new Set();
    var NorepeatTown = zoneSets.filter(item=>!set.has(item.Town)?set.add(item.Town):false);
-  console.log(NorepeatTown);
     for (let i = 0;i<NorepeatTown.length;i++) {
         txtZone += `<option value="${NorepeatTown[i].Town}">${NorepeatTown[i].Town}</option>`;
       }   
@@ -71,15 +64,20 @@ function changeHandler(val){
 //三階畫面
 function changeViewHandler(val){
   var viewSets = Object.values(rows).filter(x=>x.Town == val);
-  console.log(viewSets);
   show(viewSets);
 }
 
 //loading
 document.querySelector('#loaderbody').classList.add('hide');
-// document.querySelector('#select__area').addEventListener('change',()=>{
-//   document.querySelector('#loaderbody').classList.remove('hide');
-// });
-// setTimeout(() => {
-//   document.querySelector('#loaderbody').classList.add('hide');
-// }, 1000);
+document.querySelector('#select__area').addEventListener('change',()=>{
+  loading();
+});
+document.querySelector('#select__zone').addEventListener('change',()=>{
+  loading();
+});
+function loading(){
+  document.querySelector('#loaderbody').classList.remove('hide');
+  setTimeout(() => {
+    document.querySelector('#loaderbody').classList.add('hide');
+  }, 1000);
+}
