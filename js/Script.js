889c4e8303b7f71d1f6e61d2f147aab1cfd78669
YUAN <yankees2484@gmail.com>
2020-12-13 15:43:40 +0800

    select選取畫面顯示

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,7 @@
 const areaSelect = document.querySelector("#select__area");
 const zoneSelect = document.querySelector("#select__zone");
+var txtArea = `<option value="">請選擇行政區域</option>`;
+var txtZone = `<option value="">請選擇鄉鎮區</option>`;
 const box = document.querySelector("#box");
 const endpoint = 'public/data.json';
 
@@ -7,43 +9,30 @@ var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var rows = JSON.parse(xhttp.responseText);
-    
+    rows = JSON.parse(xhttp.responseText);
+    //去掉var變成全域變數
+
     //去重
     const set = new Set();
     const NorepeatCity = rows.filter(item=>!set.has(item.City)?set.add(item.City):false);
-   
-    ///二階select
-    var zoneval = changeHandler();
-    var zoneSets = rows.filter(function (set){
-      return set.Zone == zoneval;
-    });
-    console.log('zoneval:',zoneval);
-    console.log('zoneSets:',zoneSets);
+
+  for (let i = 0;i<NorepeatCity.length;i++) {
+    txtArea += `<option value="${NorepeatCity[i].City}">${NorepeatCity[i].City}</option>`;
+  }   
+  areaSelect.innerHTML = txtArea;
+
     //三階畫面
     var viewSets = changeViewHandler();
-    
+
     //全部顯示
-    show(NorepeatCity,zoneSets,rows);
+    show(rows);
   }
 };
 xhttp.open("GET", 'public/data.json', true);
 xhttp.send();
 
-function show(area,zone,rows){
-  var txtArea = `<option value="">請選擇行政區域</option>`;
-  var txtZone = `<option value="">請選擇鄉鎮區</option>`;
+function show(rows){
   var result = '';
-
-  for (let i = 0;i<area.length;i++) {
-    txtArea += `<option value="${area[i].City}">${area[i].City}</option>`;
-  }   
-  areaSelect.innerHTML = txtArea;
-
-  // for (let i = 0;i<zone.length;i++) {
-  //   txtZone += `<option value="${zone[i].Zone}">${zone[i].Zone}</option>`;
-  // }   
-  // areaSelect.innerHTML = txtZone;
 
   for(let j = 0;j<rows.length; j++)
     {
@@ -67,9 +56,30 @@ function show(area,zone,rows){
 
 //二階select
 function changeHandler(val){
-  return val;
+  //記得先清空原來內容
+  txtZone = `<option value="">請選擇鄉鎮區</option>`;
+   var zoneSets = Object.values(rows).filter(x=>x.City == val);
+   //去重
+   const set = new Set();
+   var NorepeatTown = zoneSets.filter(item=>!set.has(item.Town)?set.add(item.Town):false);
+  console.log(NorepeatTown);
+    for (let i = 0;i<NorepeatTown.length;i++) {
+        txtZone += `<option value="${NorepeatTown[i].Town}">${NorepeatTown[i].Town}</option>`;
+      }   
+      zoneSelect.innerHTML = txtZone;
 }
 //三階畫面
 function changeViewHandler(val){
-
+  var viewSets = Object.values(rows).filter(x=>x.Town == val);
+  console.log(viewSets);
+  show(viewSets);
 }
+
+//loading
+document.querySelector('#loaderbody').classList.add('hide');
+// document.querySelector('#select__area').addEventListener('change',()=>{
+//   document.querySelector('#loaderbody').classList.remove('hide');
+// });
+// setTimeout(() => {
+//   document.querySelector('#loaderbody').classList.add('hide');
+// }, 1000);
